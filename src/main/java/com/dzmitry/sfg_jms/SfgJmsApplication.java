5b68a8ef4fa8cc50365424e68e8f335719b66be6
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-11-05 19:52:16 +0100

    using local active mq broker with spring boot and docker

--- src/main/java/com/dzmitry/sfg_jms/SfgJmsApplication.java
+++ src/main/java/com/dzmitry/sfg_jms/SfgJmsApplication.java
@@ -1,9 +1,5 @@
 package com.dzmitry.sfg_jms;
 
-import org.apache.activemq.artemis.core.config.Configuration;
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,7 +8,7 @@ public class SfgJmsApplication {
 
 	public static void main(String[] args) throws Exception {
 
-		ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+		/*ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
 				.setPersistenceEnabled(false)
 				.setJournalDirectory("target/data/journal")
 				.setSecurityEnabled(false)
@@ -20,7 +16,7 @@ public class SfgJmsApplication {
 
 		server.start();
 
-		server.start();
+		server.start();*/
 		SpringApplication.run(SfgJmsApplication.class, args);
 	}
 
